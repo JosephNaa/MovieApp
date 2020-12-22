bb4ec91639524a229bcafa0cfd246f3d26ccf056
JosephNaa <js.pekah@gmail.com>
2020-12-22 23:26:35 +0900

    2020/12/22-v0.5

--- graphql/db.js
+++ graphql/db.js
@@ -1,26 +1,50 @@
-export const people = [
+let movies = [
     {
-        id: "0",
-        name: "Joseph",
-        age: 26,
-        gender: "male"
+        id: 0,
+        name: "Start Wars",
+        score: 1
     },
     {
-        id: "1",
-        name: "JD",
-        age: 18,
-        gender: "female"
+        id: 1,
+        name: "Avangers",
+        score: 8
     },
     {
-        id: "2",
-        name: "flynnn",
-        age: 16,
-        gender: "male"
+        id: 2,
+        name: "The Godfather",
+        score: 99
+    },
+    {
+        id: 3,
+        name: "Logan",
+        score: 2
     },
     
 ]
 
+export const getMovies = () => movies
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => String(id) === person.id)
-    return filteredPeople[0]
+    const filteredMovies = movies.filter(movies => String(id) === movie.id)
+    return filteredMovies[0]
+}
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => movie.id !== String(id))
+    if(movies.length > cleanedMovies.length){
+        movie = cleanedMovies
+        return true
+    } else{
+        return false
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    }
+    movies.push(newMovie)
+    return newMovie
 }
